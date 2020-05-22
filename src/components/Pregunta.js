fcc3b1ce77853db2d0f869c7205eac2336b8b1c3
patorma <pocontreras@ing.ucsc.cl>
2020-05-21 23:43:52 -0400

    se lee el presupuesto ingresado

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Pregunta = () => {
+  // definir el state
+  // se inicia en cero el presupuesto
+  const [cantidad, guardarCantidad] = useState(0);
+
+  // funcion que lee el presupuesto
+  // e para acceder a losd valores .target .value
+  const definirPresupuesto = (e) => {
+    // con esto lo volvemos a entero y sale azul
+    guardarCantidad(parseInt(e.target.value, 10));
+  };
+
   return (
     <>
       <h2>Coloca tu presupuesto</h2>
@@ -9,6 +20,7 @@ const Pregunta = () => {
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
         />
         <input
           type="submit"
