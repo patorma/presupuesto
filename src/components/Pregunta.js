670da48ffbc91606d577e6fe7073bc778e98c6d5
patorma <pocontreras@ing.ucsc.cl>
2020-05-22 22:51:46 -0400

    mostrando mensajes de error

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Error from "./Error";
 
 const Pregunta = () => {
   // definir el state
   // se inicia en cero el presupuesto
   const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
 
   // funcion que lee el presupuesto
   // e para acceder a losd valores .target .value
@@ -12,10 +14,26 @@ const Pregunta = () => {
     guardarCantidad(parseInt(e.target.value, 10));
   };
 
+  // Submit para definir el presupuesto
+  const agregarPresupuesto = (e) => {
+    // para que no lo envie el query string en la parte superior y tampoco recargue la pagina
+    e.preventDefault();
+
+    // Validar
+    if (cantidad < 1 || isNaN(cantidad)) {
+      guardarError(true);
+      return;
+    }
+
+    // si se pasa la valiodación
+    guardarError(false);
+  };
+
   return (
     <>
       <h2>Coloca tu presupuesto</h2>
-      <form action="">
+      {error ? <Error mensaje="El Presupuesto es Incorrecto" /> : null}
+      <form onSubmit={agregarPresupuesto}>
         <input
           type="number"
           className="u-full-width"
